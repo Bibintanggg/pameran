55bf13697de98ba27f232293ea529c00d9968f14
Bibintanggg <bintangyudhaa8@gmail.com>
2025-09-09 01:06:50 +0700

    feat: add new modal for add cards

--- resources/js/Components/AddCards.tsx
+++ resources/js/Components/AddCards.tsx
@@ -1,7 +1,25 @@
-import { PlusIcon } from "lucide-react";
+import { PlusIcon } from "lucide-react"
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogDescription,
+    DialogTrigger,
+} from "@/Components/ui/dialog"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/Components/ui/dropdown-menu"
+import { Button } from "./ui/button"
 
 interface CardsProps {
-    label: string;
+    label: string
 }
 
 export default function Cards({ label }: CardsProps) {
@@ -9,16 +27,70 @@ export default function Cards({ label }: CardsProps) {
         <div className="mt-5">
             <div className="w-40 h-16 bg-[#808080]/10 rounded-lg flex items-center justify-between px-4">
                 <div className="w-full flex items-center justify-between">
-
-                    <div className=" flex items-center justify-center">
+                    <div className="flex items-center justify-center">
                         <p className="text-black text-lg font-semibold">{label}</p>
                     </div>
 
-                    <div className="w-10 h-10 rounded-full bg-[#808080]/20 flex items-center justify-center">
-                        <button>
-                            <PlusIcon color="#215509" opacity={54} />
-                        </button>
-                    </div>
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <button className="w-10 h-10 rounded-full bg-[#808080]/20 flex items-center justify-center">
+                                <PlusIcon color="#215509" opacity={54} />
+                            </button>
+                        </DialogTrigger>
+
+                        <DialogContent className="w-96 rounded-lg">
+                            <DialogHeader>
+                                <DialogTitle className="text-start">Add {label}</DialogTitle>
+                                <DialogDescription className="text-start">
+                                    Fill in the details below to create a new card, then click Save when you're done.
+                                </DialogDescription>
+                            </DialogHeader>
+
+                            <form className="space-y-3">
+                                <div className="flex items-center gap-5">
+                                    <p>Select Currency</p>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant="outline" className="w-full text-black/50 flex">Select Your Currency</Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent className="w-56" align="start">
+
+                                            <DropdownMenuLabel>Select Your Currency</DropdownMenuLabel>
+                                            <DropdownMenuGroup>
+                                                <DropdownMenuItem>
+                                                    IDR - Indonesian Rupiah
+                                                </DropdownMenuItem>
+                                                <DropdownMenuItem>
+                                                    THB - Baht Thailand
+                                                </DropdownMenuItem>
+                                                <DropdownMenuItem>
+                                                    USD - Dollar AS
+                                                </DropdownMenuItem>
+                                            </DropdownMenuGroup>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </div>
+                                <div className="flex items-center gap-8">
+                                    <p>Card Number</p>
+                                    <input
+                                        type="number"
+                                        placeholder={`example. 6203... (min 10digit)`}
+                                        className="w-full border border-black/10 rounded p-2"
+                                    />
+                                </div>
+
+                                <div className="flex items-center justify-between">
+                                <button className="w-20 bg-red-600 text-white py-2 rounded-lg justify-end items-end">
+                                    Back
+                                </button>
+
+                                <button className="w-40 bg-slate-900 text-white py-2 rounded-lg justify-end items-end">
+                                    Save changes
+                                </button>
+                                </div>
+                            </form>
+                        </DialogContent>
+                    </Dialog>
                 </div>
             </div>
         </div>
